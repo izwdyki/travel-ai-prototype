f508b0aa69ff0004bb29124d8e394165c37e644b
izwdyki <izwdyki@gmail.com>
2025-01-12 11:36:44 +0900

    Delete src/app directory

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import TravelPlanner from '../../components/TravelPlanner';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto px-4 py-8">
-      <TravelPlanner />
-    </main>
-  );
-}
